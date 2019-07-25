78dfc42762da1832b5894428886fd02b600f22b0
吴灶洲 <>
2019-07-25 10:24:09 +0800

    “利用YYCache和AFN实现网络数据缓存“

--- WZNetWork/Classes/HZNetworking.h
+++ WZNetWork/Classes/HZNetworking.h
@@ -1,18 +1,17 @@
 //
 //  HZNetworking.h
-//  HZFrameWork
+//  HZNetWork
 //
-//  Created by 兔兔 on 2018/10/16.
-//  Copyright © 2018年 Runyalsj. All rights reserved.
+//  Created by 吴灶洲 on 2019/1/12.
+//  Copyright © 2019年 吴灶洲. All rights reserved.
 //
 
 #ifndef HZNetworking_h
 #define HZNetworking_h
 
-#import "HZRequestConst.h"
-#import "HZRequestEngine.h"
+#import "HZNetworkConfig.h"
+#import "HZNetWorkCache.h"
 #import "HZRequestManager.h"
-#import "HZURLRequest.h"
-#import "HZCacheManager.h"
+#import "HZNetworkManager.h"
 
 #endif /* HZNetworking_h */
